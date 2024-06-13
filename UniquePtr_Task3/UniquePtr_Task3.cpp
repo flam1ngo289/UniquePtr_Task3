2a389c33a836c9106c00fab544c237a53cde3085
flam1ngo <kris.s.03@mail.ru>
2024-06-13 15:33:30 +0300

    Initial commit

---
+++ UniquePtr_Task3/UniquePtr_Task3.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+template <typename T>
+class MyUniquePtr
+{
+private:
+    T* ptr;  
+
+public:
+    explicit MyUniquePtr(T* p = nullptr) : ptr(p) {}
+
+    MyUniquePtr(const MyUniquePtr&) = delete;
+
+    MyUniquePtr& operator=(const MyUniquePtr&) = delete;
+
+    T& operator*() const { return *ptr; }
+
+    T* release()
+    {
+        T* temp = ptr;
+        ptr = nullptr;
+        return temp;
+    }
+
+    ~MyUniquePtr()
+    {
+        delete ptr;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    MyUniquePtr<int> ptr(new int(10));
+    std::cout << "Значение: " << *ptr << std::endl;
+    int* raw_ptr = ptr.release(); 
+    std::cout << "Высвобожденное значение: " << *raw_ptr << std::endl;
+    delete raw_ptr; 
+
+    return 0;
+}
